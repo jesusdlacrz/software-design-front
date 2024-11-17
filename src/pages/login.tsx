4e50b2966e2e4a74c458f4f3a5b66188a892de9d
imcarlosmsX <sandovalcarlos@uninorte.edu.co>
2024-11-17 00:42:31 -0500

    update login page to use navigate function for registration link

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -146,7 +146,7 @@ const Login = () => {
         </div>
         <div className="px-8 py-4 bg-gray-700 text-center">
           <span className="text-gray-400">Don't have an account?</span> {' '}
-          <a className="font-medium text-indigo-500 hover:text-indigo-400" href="/register">
+          <a className="font-medium text-indigo-500 hover:text-indigo-400" onClick={() => navigate("/register")}>
             Sign up
           </a>
         </div>
