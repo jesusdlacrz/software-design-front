8a49eed73f59b230a417920e1c52ddc505574e2b
imcarlosmsX <sandovalcarlos@uninorte.edu.co>
2024-11-16 22:04:41 -0500

    add routing and react-query integration; implement new routes for login, registration, teams, projects, sprints, and tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Login from './pages/login';
-import Register from './pages/register';
-import Teams from './pages/teams';
-import Projects from './pages/projects'; 
-import Sprints from './pages/sprints'; 
-import Tasks from './pages/tasks';
+import {RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import {router} from './routes/routes';
 
-const AppRoutes = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/teams" element={<Teams />} />
-      <Route path="/teams/:id/projects" element={<Projects />} />
-      <Route path="/teams/:id/projects/:projectId" element={<Sprints />} />
-      <Route path="/projects/:projectId/sprints/:sprintId" element={<Tasks />} /> {/* Ruta de Tareas */}
-    </Routes>
-  </BrowserRouter>
-);
 
-export default AppRoutes;
+const queryClient = new QueryClient();
+
+const App: React.FC = () => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <div className="app">
+                <RouterProvider router={router} />
+            </div>
+        </QueryClientProvider>
+    );
+};
+
+export default App;
