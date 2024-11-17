6f9cb9a82442ba5d7aabba7d9c58b611bb47e19b
imcarlosmsX <sandovalcarlos@uninorte.edu.co>
2024-11-17 00:03:17 -0500

    refactor routing configuration; streamline import statements and improve code readability

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,35 +1,34 @@
-import { createBrowserRouter } from "react-router-dom";
-import Login from "../pages/login";
-import Register from "../pages/register";
-import Projects from "../pages/projects";
-import Sprints from "../pages/sprints";
-import Tasks from "../pages/tasks";
-import Teams from "../pages/teams";
-export const router = createBrowserRouter(
-  [
-    {
-      path: `/login`,   
-      element: <Login />,
-    },
-    {
-      path: `/register`,
-      element: <Register />,
-    },
-    {
-        path: `/teams`,
-        element: <Teams />,
-    },
-    {
-      path: `/teams/:id/projects`,
-      element: <Projects />,
-    },
-    {
-      path: `/teams/:id/projects/:projectId`,
-      element: <Sprints />,
-    },
-    {
-        path: `/projects/:projectId/sprints/:sprintId`,
-        element: <Tasks />,
-    }
-  ],
-);
+import { createBrowserRouter } from 'react-router-dom';
+import Register from '../pages/register';
+import Login from '../pages/login';
+import Teams from '../pages/teams';
+import Projects from '../pages/projects';
+import Sprints from '../pages/sprints';
+import Tasks from '../pages/tasks';
+
+export const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/teams',
+    element: <Teams />,
+  },
+  {
+    path: '/teams/:id/projects',
+    element: <Projects />,
+  },
+  {
+    path: '/teams/:id/projects/:projectId',
+    element: <Sprints />,
+  },
+  {
+    path: '/projects/:projectId/sprints/:sprintId',
+    element: <Tasks />,
+  },
+]);
