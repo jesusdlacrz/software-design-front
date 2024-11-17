6ba859ae5a65a1613e6b3545f825eb87704cb9df
imcarlosmsX <sandovalcarlos@uninorte.edu.co>
2024-11-17 00:25:39 -0500

    update footer copyright text from MyApp to ScrumManager

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -132,7 +132,7 @@ const LandingPage = () => {
       <footer className="bg-gray-800 text-white py-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between">
           <p className="text-sm text-gray-400">
-            &copy; 2024 MyApp. All rights reserved.
+            &copy; 2024 ScrumManager. All rights reserved.
           </p>
           <div className="space-x-4">
             <a
